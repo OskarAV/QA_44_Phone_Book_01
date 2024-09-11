052cbed83dc12e7e4a906e6f87ac555cc37829f8
OskarAV <afonin.viktor@gmail.com>
2024-09-11 13:47:53 +0300

    Created project structure

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,14 @@
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    static WebDriver driver;
+    public static void setDriver(WebDriver wd) {
+        driver = wd;
+    }
+
+
+}
